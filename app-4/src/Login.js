fc3c56a9d4a36da96372a3ac3acdc106f632d4f2
dhansenaz <dhansen@allthingsdot.com>
2018-03-19 18:08:48 -0700

    final

--- app-4/src/Login.js
+++ app-4/src/Login.js
@@ -1,38 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
-class Login extends Component{
-    constructor(){
-        super()
+class Login extends Component {
+  constructor() {
+    super();
 
-        this.state = {
-            username: '',
-            password: ''
-        };
-        //bind here
-       
-     }    
-     handleUserName(name){
-         this.setState({username: name})
+    this.state = {
+      username: "",
+      password: ""
+    };
 
-     }
-     HandlePasswordChange(pass){
-         this.setState({password:pass})
+    this.handleLogin = this.handleLogin.bind( this );
+  }
 
-     }
-     handleLogin(){
-         alert(`Username: $`)
+  handleUsernameChange( name ) {
+    this.setState({ username: name });
+  }
 
-     }
-     render(){
-        return(
-            <div>
-            <input onChange ={ (e ) => this.handleUsernameChange(e.target.value)} type='text'/>
-            <input onChange ={ (e) => this.HandlePasswordChange(e.target.value)} type="text" />
-            <button onClick = {this.handleLogin}>Login</button>
-            </div>
-            
+  handlePasswordChange( pass ) {
+    this.setState({ password: pass });
+  }
 
-        )
-    }
+  handleLogin() {
+    alert(`Username: ${ this.state.username } Password: ${ this.state.password }`);
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={ (e) => this.handleUsernameChange( e.target.value ) } type="text"/>
+        <input onChange={ (e) => this.handlePasswordChange( e.target.value ) } type="text"/>
+        <button onClick={ this.handleLogin }>Login</button>
+      </div>
+    )
+  }
 }
-export default Login
+
+export default Login;
